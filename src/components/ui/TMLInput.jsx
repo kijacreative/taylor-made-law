021b86b223a0bb48df33ad4d269b1545c10446fa
kiel <kiel@kijacreative.com>
2026-02-18 22:31:47 +0000

    Manual code change

--- src/components/ui/TMLInput.jsx
+++ src/components/ui/TMLInput.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 
-const TMLInput = React.forwardRef(({ 
+const TMLInput = React.forwardRef(({
   label,
   error,
   helperText,
   className,
   containerClassName,
   required,
-  ...props 
+  ...props
 }, ref) => {
   return (
     <div className={cn('space-y-1.5', containerClassName)}>
-      {label && (
-        <label className="block text-sm font-medium text-gray-700">
-          {label}
-          {required && <span className="text-red-500 ml-1">*</span>}
-        </label>
-      )}
+      
+
+
+
+
+
       <input
         ref={ref}
         className={cn(
@@ -29,16 +29,16 @@ const TMLInput = React.forwardRef(({
           error && 'border-red-500 focus:ring-red-500/20 focus:border-red-500',
           className
         )}
-        {...props}
-      />
-      {error && (
-        <p className="text-sm text-red-600">{error}</p>
-      )}
-      {helperText && !error && (
-        <p className="text-sm text-gray-500">{helperText}</p>
-      )}
-    </div>
-  );
+        {...props} />
+
+      {error &&
+      <p className="text-sm text-red-600">{error}</p>
+      }
+      {helperText && !error &&
+      <p className="text-sm text-gray-500">{helperText}</p>
+      }
+    </div>);
+
 });
 
 TMLInput.displayName = 'TMLInput';
