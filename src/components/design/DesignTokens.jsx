28e325fb6542be2896588f6201c62800ad73a606
Kiel Jared <kiel@kijacreative.com>
2026-03-03 06:42:58 +0000

    File changes

--- src/components/design/DesignTokens.jsx
+++ src/components/design/DesignTokens.jsx
@@ -109,6 +109,16 @@ export const borderRadius = {
 };
 
 // Practice areas for the platform
+export const US_STATES = [
+  'Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware',
+  'Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky',
+  'Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi',
+  'Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico',
+  'New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania',
+  'Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont',
+  'Virginia','Washington','West Virginia','Wisconsin','Wyoming'
+];
+
 export const PRACTICE_AREAS = [
   'Personal Injury',
   'Medical Malpractice',
