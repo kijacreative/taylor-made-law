586d8cd4cf4b870516841d0e16c728a35b4f0f7c
Kiel Jared <kiel@kijacreative.com>
2026-03-10 07:20:39 +0000

    File changes

--- src/pages/GroupDetail.jsx
+++ src/pages/GroupDetail.jsx
@@ -83,7 +83,7 @@ export default function GroupDetail() {
     navigate(createPageUrl('Groups'));
   };
 
-  if (loading || circleLoading || membershipLoading) {
+  if (loading || circleLoading || membershipLoading || (!myMembership && !!circleId && !!user?.id)) {
     return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         <Loader2 className="w-8 h-8 animate-spin text-[#3a164d]" />
